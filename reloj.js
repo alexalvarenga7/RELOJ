bc3784707a673695a28918a9276bdd1d4944cd7d
alexalvarenga7 <2000zelaya32@gmail.com>
2026-04-07 22:49:49 -0600

    Primer commit

---
+++ reloj.js
@@ -0,0 +1,18 @@
+const secondHand = document.querySelector(".seconds");
+const minuteHand = document.querySelector(".minutes");
+const hourHand = document.querySelector(".hours");
+
+const moveHands = () => {
+    const date = new Date();
+    console.log(date);
+    const seconds = date.getSeconds() / 60;
+    console.log(seconds + date.getMinutes() / 60);
+    const minutes = (seconds + date.getMinutes()) / 60;
+    const hours = (minutes + date.getHours()) / 12;
+
+    secondHand.style.setProperty("--rotate", seconds * 360);
+    minuteHand.style.setProperty("--rotate", minutes * 360);
+    hourHand.style.setProperty("--rotate", hours * 360);
+};
+
+setInterval(moveHands, 1000);
